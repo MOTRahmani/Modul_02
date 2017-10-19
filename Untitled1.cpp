d66ae9210c2e7bce9f7175462b956d88218ca5f6
MOTRahmani <muhammadrahmani84@gmail.com>
2017-10-19 20:03:19 +0800

    Perbaikan
    Perbaikan

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main (){
-	nilai=nilai+1;
 	int nilai;
-	int 1Nilai;
+	int Nilai;
+	
+	nilai=nilai+1;
+	Nilai=Nilai+1;
 	
 	cout << nilai;
-	cout << 1Nilai;
+	cout << Nilai;
 }
